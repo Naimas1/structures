1b3b8a8120f99501041791097e8de5a51a36a7cf
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-03 19:52:49 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,51 @@
-﻿namespace structures
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structures
 {
-    internal class Program
+    struct DecimalNumber
     {
-        static void Main(string[] args)
+        private int decimalValue;
+
+        public DecimalNumber(int value)
         {
-            Console.WriteLine("Hello, World!");
+            decimalValue = value;
+        }
+
+        public string ToBinary()
+        {
+            return Convert.ToString(decimalValue, 2);
+        }
+
+        public string ToOctal()
+        {
+            return Convert.ToString(decimalValue, 8);
+        }
+
+        public string ToHexadecimal()
+        {
+            return Convert.ToString(decimalValue, 16);
+        }
+
+        public override string ToString()
+        {
+            return decimalValue.ToString();
+        }
+    }
+
+    class Programas
+    {
+        static void Main()
+        {
+            DecimalNumber decimalNumber = new DecimalNumber(42);
+
+            Console.WriteLine("Decimal Number: " + decimalNumber);
+            Console.WriteLine("Binary: " + decimalNumber.ToBinary());
+            Console.WriteLine("Octal: " + decimalNumber.ToOctal());
+            Console.WriteLine("Hexadecimal: " + decimalNumber.ToHexadecimal());
         }
     }
 }
